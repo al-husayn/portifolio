3b0e3ea6d4bfa77343468136cc5ec0b865aab056
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2022-06-15 15:54:19 +0000

    Higher order Components created

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: "7xggontm",
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: "sk97Gj9G43SPlqqSHe4upakl9200OQu292yIApD9yufn4TKQFgZRCcD80a0qW72sQeUKf0Ohi2Z5e0h4qAQWQJTYZnZLcmWUO2q0t5PlP1vMmr10lRKncov4DEYgv5ej46gKaSfBxALQSwOd9fVFxhYvAZe2CPJOnQRoavsgzvy3IetTLmX3",
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
